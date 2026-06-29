ef985965505b735392a0d16eaddc7115581deccc
Doraking <120563040+dorakingx@users.noreply.github.com>
2026-06-29 10:25:12 +0900

    Set browser tab title to Technical Portfolio

--- app/(redirect)/layout.tsx
+++ app/(redirect)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "../globals.css";
 
 export const metadata: Metadata = {
-  title: "Doraking Portfolio",
+  title: "Technical Portfolio",
   robots: { index: false }
 };
 
